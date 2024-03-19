531f7ba0f0d6fbfac834c94be71022eab9160bd6
mikotonori <142949766+mikotonori@users.noreply.github.com>
2024-03-19 13:54:08 +0100

    Update Mono_landingPagePrivacyNavRemove.js

--- Mono_landingPagePrivacyNavRemove.js
+++ Mono_landingPagePrivacyNavRemove.js
@@ -1,7 +1,11 @@
-// Kode ment for å bli brukt i Custom HTML på Personvern-siden
-// Formålet med koden er å fjerne hamburgermenyikonet fra personvernsiden til énsidere, hvor dette ikke er nødvendig å ha
+/*
+Kode ment for å bli brukt i Custom HTML på Personvern-siden.
 
-// <script></script> skal omkranse koden hvis brukt i HTML
+Formålet med koden er å fjerne hamburgermenyikonet fra personvernsiden til énsidere, hvor dette ikke er nødvendig å ha.
+
+
+OBS: <script></script> skal omkranse koden hvis brukt i HTML
+*/
 
 document.addEventListener('DOMContentLoaded', function(e) {
   document.querySelector('.module.nav').style.display = 'none';
