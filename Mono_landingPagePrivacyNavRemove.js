1987b5f3ee3f428bcaac3e91f7d1aee285306298
mikotonori <142949766+mikotonori@users.noreply.github.com>
2024-03-19 12:23:41 +0100

    Add files via upload

---
+++ Mono_landingPagePrivacyNavRemove.js
@@ -0,0 +1,8 @@
+// Kode ment for å bli brukt i Custom HTML på Personvern-siden
+// Formålet med koden er å fjerne hamburgermenyikonet fra personvernsiden til énsidere, hvor dette ikke er nødvendig å ha
+
+// <script></script> skal omkranse koden hvis brukt i HTML
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  document.querySelector('.module.nav').style.display = 'none';
+});
