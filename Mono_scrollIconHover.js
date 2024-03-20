cdb7e10676c9cdbb43857f8f0c073a2b3b241e3a
mikotonori <142949766+mikotonori@users.noreply.github.com>
2024-03-20 14:34:03 +0100

    Update Mono_scrollIconHover.js

--- Mono_scrollIconHover.js
+++ Mono_scrollIconHover.js
@@ -1,14 +1,24 @@
 /*
-Lar deg definere color i klassen .scrollIconHover i custom CSS slik at pilen i scrolleknappen skifter farge når du holder musen over
+Lar deg definere color i klassen .scrollIconHover i custom CSS slik at pilen i scrolleknappen skifter farge når du holder musen over.
 
-CSS for å stille på scrolleknappens ikon:
+Lar deg også definere utseende til scrolleknappen og -ikonet når de blir fokusert i klassen .scrollIconFocus.
+
+CSS for å stille på scrolleknappens pilikon når musa går over den:
 
 .scrollIconHover::before {
 	color: #101010 !important;
 }
 
+CSS for å stille på scrolleknappen og dens pilikon når de fokuseres:
 
-OBS: <script></script> skal omkranse koden hvis brukt i HTML
+.scrollIconFocus {
+	background-color: #fff !important;
+  	border: 1px solid #101010 !important;
+}
+
+.scrollIconFocus span::before {
+	color: #101010 !important;
+}
 */
 
 document.addEventListener('DOMContentLoaded', function(e) {
@@ -21,5 +31,13 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
   scrollIcon.addEventListener('mouseleave', function(e) {
     scrollIconSpan.classList.remove('scrollIconHover');
+  });
+
+  scrollIcon.addEventListener('focus', function(e) {
+    scrollIcon.classList.add('scrollIconFocus');
+  });
+
+  scrollIcon.addEventListener('blur', function(e) {
+    scrollIcon.classList.remove('scrollIconFocus');
   });
 });
