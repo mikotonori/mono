db0efe01caaf1c27e6e81ada77c3830d380df67b
mikotonori <ol.he.ho.da@gmail.com>
2024-04-03 22:20:42 +0200

    Progress

--- Mono_grid.js
+++ Mono_grid.js
@@ -1,33 +1,48 @@
-document.addEventListener('DOMContentLoaded', function(e){
-    const gridConfig = {
-        direction: 'right',
-        gridColumnSize: 6, // 1 til 12
-        gridColumnCount: null,
-        gridRowCount: null,
-        columnGap: 0,
-        rowGap: 0,
-    };
+document.addEventListener("DOMContentLoaded", function (e) {
+  const gridConfig = {
+    direction: "right",
+    parentColumnSize: 6, // 1 til 12
+    gridColumnCount: null,
+    gridColumnMinWidth: "100px",
+    gridRowCount: null,
+    columnGap: 0,
+    rowGap: 0,
+  };
 });
 
 function createGridOnSide(config) {
-    document.querySelectorAll(`.cc_grid-to-${leftOrRight}`).forEach(elementOnSide => {
-        const row = elementOnSide.closest('.row');
+  document
+    .querySelectorAll(`.cc_grid-to-${leftOrRight}`)
+    .forEach((elementOnSide) => {
+      const row = elementOnSide.closest(".row");
 
-        const gridColumns = row.querySelectorAll('.col:not(.cc_no-grid)');
-        const gridColumnsOuterHTML = [];
+      const gridColumns = row.querySelectorAll(
+        ".col:not(.cc_no-grid):not(.cc_grid-to-left):not(.cc_grid-to-right)"
+      );
+      const gridColumnsOuterHTML = [];
 
-        gridColumns.forEach(col => {
-            gridColumnsOuterHTML.push(col.outerHTML);
-            col.remove();
-        });
+      gridColumns.forEach((col) => {
+        gridColumnsOuterHTML.push(col.outerHTML);
+        col.remove();
+      });
 
-        elementOnSide.insertAdjacentHTML('afterend', html`<button>`);
-
-
-        if (leftOrRight === 'right') {
-            elementOnSide.insertAdjacentHTML('afterend', html`
-                <div class="col col-sm-${config.gridColumnSize}" style="display: grid; grid-template-columns: ${config.gridColumnCount ?? auto}; grid-template-rows: ${config.gridRowCount ?? auto}; "
-            `);
-        }
+      if (leftOrRight === "right") {
+        elementOnSide.insertAdjacentHTML(
+          "afterend",
+          /*html*/ `
+            <div
+              class="col col-sm-${config.parentColumnSize}"
+              style="
+                display: grid;
+                grid-template-columns: repeat(${
+                  config.gridColumnCount ?? "auto-fill"
+                }, minmax(${gridConfig.gridColumnMinWidth}, 1fr));
+                grid-template-rows: ${config.gridRowCount ?? "auto"};
+                column-gap: ${config.columnGap};
+                row-gap: ${config.rowGap};
+              ">${gridColumnsOuterHTML.join("")}</div>
+          `
+        );
+      }
     });
 }
