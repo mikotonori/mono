7f02ff5ff62aa186f66a16f639d7d6d99347c79a
mikotonori <ol.he.ho.da@gmail.com>
2024-04-04 21:25:00 +0200

    Small update

--- Mono_grid.js
+++ Mono_grid.js
@@ -1,48 +1,60 @@
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener('DOMContentLoaded', function (e) {
   const gridConfig = {
-    direction: "right",
+    direction: 'right', // Need to lose this
     parentColumnSize: 6, // 1 til 12
     gridColumnCount: null,
-    gridColumnMinWidth: "100px",
+    gridColumnMinWidth: '100px',
     gridRowCount: null,
     columnGap: 0,
     rowGap: 0,
+    parentColumnPadding: 0,
   };
+
+  createGridOnSide(gridConfig);
 });
 
 function createGridOnSide(config) {
   document
-    .querySelectorAll(`.cc_grid-to-${leftOrRight}`)
-    .forEach((elementOnSide) => {
-      const row = elementOnSide.closest(".row");
+    .querySelectorAll(`.cc_grid-to-${config.direction}`)
+    .forEach(elementOnSide => {
+      const row = elementOnSide.closest('.row');
 
       const gridColumns = row.querySelectorAll(
-        ".col:not(.cc_no-grid):not(.cc_grid-to-left):not(.cc_grid-to-right)"
+        '.col:not(.cc_no-grid):not(.cc_grid-to-left):not(.cc_grid-to-right):not([style*="display: grid"]):not(.module)'
       );
       const gridColumnsOuterHTML = [];
 
-      gridColumns.forEach((col) => {
-        gridColumnsOuterHTML.push(col.outerHTML);
+      gridColumns.forEach(col => {
+        const newOuter = col.outerHTML.replace(
+          '<div',
+          '<div style="width: 100%;"'
+        );
+
+        gridColumnsOuterHTML.push(newOuter);
         col.remove();
       });
 
-      if (leftOrRight === "right") {
-        elementOnSide.insertAdjacentHTML(
-          "afterend",
-          /*html*/ `
-            <div
-              class="col col-sm-${config.parentColumnSize}"
-              style="
-                display: grid;
-                grid-template-columns: repeat(${
-                  config.gridColumnCount ?? "auto-fill"
-                }, minmax(${gridConfig.gridColumnMinWidth}, 1fr));
-                grid-template-rows: ${config.gridRowCount ?? "auto"};
-                column-gap: ${config.columnGap};
-                row-gap: ${config.rowGap};
-              ">${gridColumnsOuterHTML.join("")}</div>
-          `
-        );
+      const gridHTML = /*html*/ `
+        <div
+          class="col col-sm-${config.parentColumnSize}"
+          style="
+            display: grid;
+            grid-template-columns: repeat(${
+              config.gridColumnCount ?? 'auto-fill'
+            }, minmax(${config.gridColumnMinWidth}, 1fr));
+            grid-template-rows: repeat(${config.gridRowCount ?? '2'}, 1fr);
+            column-gap: ${config.columnGap};
+            row-gap: ${config.rowGap};
+            padding: ${config.parentColumnPadding};
+          ">${gridColumnsOuterHTML.join('')}</div>
+      `;
+
+      if (config.direction === 'right') {
+        elementOnSide.insertAdjacentHTML('afterend', gridHTML);
+      }
+
+      if (config.direction === 'left') {
+        elementOnSide.insertAdjacentHTML('beforebegin', gridHTML);
       }
     });
 }
