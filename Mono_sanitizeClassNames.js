65fac2831884f6903d4f5edc86b185fb513ab48b
mikotonori <142949766+mikotonori@users.noreply.github.com>
2024-04-22 10:47:55 +0200

    Add files via upload

---
+++ Mono_sanitizeClassNames.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const listOfClassNamesToRemove = [
+    'klasser',
+    'du-vil-fjerne',
+    'fra-alle-elementer',
+    'på-siden',
+  ];
+
+  listOfClassNamesToRemove.forEach(className => {
+    const allElementsWithClass = document.querySelectorAll(`.${className}`);
+    allElementsWithClass.forEach(el => el.classList.remove(className));
+  });
+
+  const classRemoveMap = {
+    etKlasseNavn: 'klasse-som-fjernes',
+    etAnnetKlasseNavn: [
+      'klasser',
+      'du-vil',
+      'fjerne-fra-elementer',
+      'med-klassen-etAnnetKlasseNavn',
+    ],
+  };
+
+  const removalSources = Object.keys(classRemoveMap);
+
+  removalSources.forEach(removalSource => {
+    const removalTarget = classRemoveMap[removalSource];
+    const removalSourceEls = document.querySelectorAll(`.${removalSource}`);
+
+    removalSourceEls.forEach(removalSourceEl => {
+      if (typeof removalTarget === 'string') {
+        removalSourceEl.classList.remove(removalTarget);
+      } else if (removalTarget instanceof Array) {
+        removalTarget.forEach(target => {
+          removalSourceEl.classList.remove(target);
+        });
+      }
+    });
+  });
+});
