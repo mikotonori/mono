de39a00d61238c0ae481a95e0bdce2e298c3accd
mikotonori <142949766+mikotonori@users.noreply.github.com>
2024-04-26 11:09:35 +0200

    Add files via upload

---
+++ Mono_directlyApplyStylesInHTML.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const elMap = [
+    [
+      '#CSS .selector ~ skrives + her',
+      [
+        'rule: value;',
+        'rule: value;',
+        'rule: value;',
+        'rule: value;',
+        'rule: value;',
+      ],
+    ],
+    [
+      '#id',
+      [
+        'rule: value;',
+        'rule: value;',
+        'rule: value;',
+        'rule: value;',
+        'rule: value;',
+      ],
+    ],
+    [
+      '.class ~ siblingElement.siblingClass',
+      [
+        'rule: value;',
+        'rule: value;',
+        'rule: value;',
+        'rule: value;',
+        'rule: value;',
+      ],
+    ],
+  ];
+
+  elMap.forEach(elStyleSet => {
+    const els = document.querySelectorAll(elStyleSet[0]);
+
+    els.forEach(el => {
+      const stylesForApplication = elStyleSet[1].reduce((styles, style) => {
+        styles = styles.concat(' ', style);
+        return styles;
+      }, '');
+
+      const originalStyle = el.getAttribute('style');
+
+      const newStyleAttribute =
+        originalStyle?.concat(' ', stylesForApplication) ??
+        stylesForApplication;
+
+      el.setAttribute('style', newStyleAttribute);
+    });
+  });
+});
